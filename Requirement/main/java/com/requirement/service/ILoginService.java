7069a35257be64f2b7161b95870cc5e6ebb8f19c
benoyprakash <benoyprakash@gmail.com>
2016-05-28 12:59:22 +0530

    removlas

--- Requirement/main/java/com/requirement/service/ILoginService.java
+++
@@ -1,9 +0,0 @@
-package com.requirement.service;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface ILoginService {
-
-	public String authenticateUser(String username , String password);
-}
