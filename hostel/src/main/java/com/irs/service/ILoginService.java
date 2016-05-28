6873e9fe2696899f4d8f48bada889c0069373a0b
benoyprakash <benoyprakash@gmail.com>
2016-05-28 12:57:32 +0530

    removal

--- hostel/src/main/java/com/irs/service/ILoginService.java
+++
@@ -1,9 +0,0 @@
-package com.irs.service;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface ILoginService {
-
-	public String authenticateUser(String username , String password);
-}
--- hostel/src/main/java/com/irs/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.irs.utils;
-
-public class Constants {
-
-	public static final String ONE = "1";
-}
